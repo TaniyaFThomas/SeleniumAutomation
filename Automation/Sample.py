9d03923cb532b1e49868e28350fc11790b7ca01e
TaniyaFThomas <taniya.f.thomas94@gmail.com>
2021-09-18 17:35:15 +0530

    Selenium tests

--- Automation/Sample.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-
-#driver = webdriver.Chrome("C:\\Users\\taniya.thomas\\Downloads\\chromedriver_win32_94\\chromedriver")
-driver = webdriver.Chrome()
-driver.maximize_window()
-driver.get("https://www.google.com/")
-driver.implicitly_wait(5)
-driver.find_element_by_name("q").send_keys("test")
-driver.close()
